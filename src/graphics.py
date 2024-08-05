46b3a0156403dfaae9c8e897d6f563b37bbf176f
GregPolack <grzelock@gmail.com>
2024-08-05 16:07:57 +0200

    add draw_move method

--- src/graphics.py
+++ src/graphics.py
@@ -5,13 +5,12 @@ class Window:
         
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
         self.__canvas = Canvas(self.__root, bg='white', height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
 
         self.__is_running = False
-
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
         self.__root.update_idletasks()
@@ -43,36 +42,55 @@ class Line:
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
 
 class Cell:
-    def __init__(self, win, top_left, bot_right):
+    def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bot_wall = True
         
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+
+        self._win = win
+
+    def draw(self, top_left, bot_right):
         self._x1 = top_left.x
         self._y1 = top_left.y
         self._x2 = bot_right.x
         self._y2 = bot_right.y
-
-        self._win = win
-
-    def draw(self):
+        
         if self.has_left_wall:
             left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
-
             self._win.draw_line(left_wall)
         
         if self.has_right_wall:
             right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
-
             self._win.draw_line(right_wall)
 
         if self.has_top_wall:
             top_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
-
             self._win.draw_line(top_wall)
 
         if self.has_bot_wall:
             bot_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-
             self._win.draw_line(bot_wall)
+    
+    def draw_move(self, to_cell, undo=False):
+        x_center = (self._x1 + self._x2)/2
+        y_center = (self._y1 + self._y2)/2
+        self._center = Point(x_center, y_center)
+
+        x_center2 = (to_cell._x1 + to_cell._x2)/2
+        y_center2 = (to_cell._y1 + to_cell._y2)/2
+        to_cell._center = Point(x_center2, y_center2)
+        
+        move_line = Line(self._center, to_cell._center)
+
+        if undo == False:
+            fill_color = 'red'
+        else:
+            fill_color = 'gray'
+        
+        self._win.draw_line(move_line, fill_color=fill_color)
--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from graphics import *
 
 def main():
     win = Window(1024, 768)
+    
+    c1 = Cell(win)
+    c1.draw(Point(50, 50), Point(70, 30))
 
-    cell = Cell(win, Point(50, 50), Point(70, 30))
-    cell.draw()
+    c2 = Cell(win)
+    c2.draw(Point(100, 100), Point(130, 70))
 
-    cell = Cell(win, Point(100, 100), Point(130, 70))
-    cell.draw()
+    c1.draw_move(c2)
 
     win.wait_for_close()
 
