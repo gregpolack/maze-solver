1a54b1d973e6818c3a48361d1fe7c770d7b0c85c
GregPolack <grzelock@gmail.com>
2024-08-05 13:40:28 +0200

    add class to draw lines

--- src/graphics.py
+++ src/graphics.py
@@ -9,7 +9,7 @@ class Window:
         self.__canvas = Canvas(self.__root, bg='white', height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
 
-        self.is_running = False
+        self.__is_running = False
 
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
@@ -18,15 +18,26 @@ class Window:
         self.__root.update()
     
     def wait_for_close(self):
-        self.is_running = True
+        self.__is_running = True
         
         while self.__is_running == True:
             self.redraw()
     
     def close(self):
-        self.is_running = False
+        self.__is_running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
-    win = Window(800, 600)
-    win.wait_for_close()
+    win = Window(1024, 768)
 
+    line_one = Line(Point(50, 50), Point(90, 110))
+    win.draw_line(line_one, "black")
+    
+    line_two = Line(Point(100, 120), Point(150, 180))
+    win.draw_line(line_two, "red")
+
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
