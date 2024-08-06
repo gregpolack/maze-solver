43c40f07f7223b56d632bf3932c15b18f22944a7
GregPolack <grzelock@gmail.com>
2024-08-06 07:33:08 +0200

    add Maze class

--- src/main.py
+++ src/main.py
@@ -1,17 +1,13 @@
-from graphics import *
+from graphics import Window
+from maze import Maze
 
 def main():
+    
     win = Window(1024, 768)
     
-    c1 = Cell(win)
-    c1.draw(Point(50, 50), Point(70, 30))
-
-    c2 = Cell(win)
-    c2.draw(Point(100, 100), Point(130, 70))
-
-    c1.draw_move(c2)
-
+    Maze(50, 50, 13, 19, 50, 50, win)
+    
     win.wait_for_close()
-
+    
 if __name__ == "__main__":
     main()
---
+++ src/maze.py
@@ -0,0 +1,48 @@
+from graphics import Cell, Point
+import time
+
+class Maze:
+    def __init__(self, x1, y1, 
+                num_rows, num_cols, 
+                cell_size_x, cell_size_y, 
+                win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+
+        self._create_cells()
+    
+    def _create_cells(self):
+        self._cells = []
+
+        for _ in range(self.num_cols):
+            col = []
+            for _ in range(self.num_rows):
+                col.append(Cell(self.win))
+            
+            self._cells.append(col)
+        
+        # i == index of column, j == index of cell.
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+
+        top_left = Point(self.x1 + (i * self.cell_size_x), 
+                         self.y1 + (j * self.cell_size_y))
+        bot_right = Point((self.x1 + self.cell_size_x) + (i * self.cell_size_x),
+                           self.y1 + self.cell_size_y + (j * self.cell_size_y))
+
+        cell = Cell(self.win)
+        cell.draw(top_left, bot_right)
+
+        self._animate()
+    
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
